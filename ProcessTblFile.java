fe5125f1207b5c655177b919cf99dfc8ea6b4f2b
Liyuan Bi <18209223080@163.com>
2017-04-14 13:17:30 +0800

    Process TPC-H data for postgre
    A java file

---
+++ ProcessTblFile.java
@@ -0,0 +1,42 @@
+package bly;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProcessTblFile {
+	public ProcessTblFile(Scanner in) {  
+		String line = "";
+		while(in.hasNextLine()){
+			line = in.nextLine();
+			StringBuilder newLine = new StringBuilder(line);
+			int len = newLine.length();
+			if(newLine.charAt(len-1)=='|'){
+				newLine.deleteCharAt(len-1);
+        
+			}
+			System.out.println(newLine.toString()); 	
+		}
+	}  
+
+
+	public static void main(String[] args) throws Exception{  
+        // TODO 自动生成的方法存根  
+        List<String> fileName = new ArrayList<>(Arrays.asList("customer.tbl","lineitem.tbl","nation.tbl","orders.tbl","part.tbl","partsupp.tbl","region.tbl","supplier.tbl"));
+        String inputFilenamePrefix = "/Users/biliyuan/Documents/workspace/Postgre/files/";  
+        String outputFilenamePrefix = "/Users/biliyuan/Documents/workspace/Postgre/files/output/"; 
+        
+        for(int i=0;i<fileName.size();i++){
+        	
+            System.setIn(new FileInputStream(inputFilenamePrefix + fileName.get(i)));  
+            System.setOut(new PrintStream(new FileOutputStream(outputFilenamePrefix + fileName.get(i))));  
+            Scanner in = new Scanner(System.in);  
+            ProcessTblFile ex = new ProcessTblFile(in);  
+            in.close();
+        } 
+    }  
+}
